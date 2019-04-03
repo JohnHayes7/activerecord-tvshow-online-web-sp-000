38b7873096f878d811c3c3d6b77bf361584857e0
John Hayes <johnnyhayes7@gmail.com>
2019-04-03 17:46:18 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,20 @@
 class Show < ActiveRecord::Base
   
+  def self.highest_rating
+    Show.maximum(:rating)
+  end
+  
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+  
+  def self.most_popular_show
+    most_pop = Show.find_by(highest_rating)
+  end
+  
+  def self.least_popular_show
+    least_pop = Show.find_by(lowest_rating)
+    binding.pry
+  end
+  
 end
